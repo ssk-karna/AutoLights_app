c80b3abf148a2871f3427f01f165a2079219184b
ssk-karna <satyamshashankkaran.ssk@gmail.com>
2020-09-23 15:42:50 +0530

    SignUp Activity Layout

--- app/src/main/java/com/truedevelopment/autolight/MainActivity.java
+++ app/src/main/java/com/truedevelopment/autolight/MainActivity.java
@@ -2,9 +2,13 @@ package com.truedevelopment.autolight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -41,7 +45,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent  = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View,String>(splashLogo,"logo_image");
+
+                ActivityOptions options =  ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
                 finish();
             }
         },SPLASH_SCREEN);
