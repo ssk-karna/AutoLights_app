65fa89bf9ac22abdd4a8e5c9bc9492ba895fb7d9
ssk-karna <satyamshashankkaran.ssk@gmail.com>
2020-09-24 10:56:46 +0530

    Register User in Firebase

--- app/src/main/java/com/truedevelopment/autolight/LoginActivity.java
+++ app/src/main/java/com/truedevelopment/autolight/LoginActivity.java
@@ -2,13 +2,62 @@ package com.truedevelopment.autolight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button callSignUp,login;
+    TextView mWelcome, mSignIn;
+    TextInputLayout mUserId, mPassword;
+    ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.btnSignUp);
+        login = findViewById(R.id.btnLogin);
+        mWelcome = findViewById(R.id.welcomeText);
+        mSignIn = findViewById(R.id.signInText);
+        mUserId = findViewById(R.id.username);
+        mPassword = findViewById(R.id.password);
+        logoImage = findViewById(R.id.logoImage);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this,SignUp.class);
+
+                Pair[] pairs = new Pair[7];
+
+                pairs[0] = new Pair<View,String>(logoImage,"logo_image");
+                pairs[1] = new Pair<View,String>(callSignUp,"trans_new_user");
+                pairs[2] = new Pair<View,String>(login,"trans_login");
+                pairs[3] = new Pair<View,String>(mWelcome,"trans_welcome");
+                pairs[4] = new Pair<View,String>(mSignIn,"trans_signinText");
+                pairs[5] = new Pair<View,String>(mUserId,"trans_email");
+                pairs[6] = new Pair<View,String>(mPassword,"trans_password");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+
+                startActivity(intent,options.toBundle());
+
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/truedevelopment/autolight/SignUp.java
+++ app/src/main/java/com/truedevelopment/autolight/SignUp.java
@@ -3,12 +3,139 @@ package com.truedevelopment.autolight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+
+  FirebaseDatabase rootNode;
+  DatabaseReference reference;
+
+  TextInputLayout regName, regMail,regPhone, regPassword;
+  Button register,backToLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //FirebaseApp.initializeApp(this);
+
+        regName = findViewById(R.id.name);
+        regMail = findViewById(R.id.email);
+        regPassword = findViewById(R.id.passwordSignUp);
+        register = findViewById(R.id.btnRegister);
+        regPhone = findViewById(R.id.phone);
+        backToLogin = findViewById(R.id.btnLoginPage);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser(v);
+            }
+        });
+
+
+    }
+
+    private Boolean validateName(){
+        String val = regName.getEditText().getText().toString();
+
+        if(val.isEmpty()){
+            regName.setError("Field Cannot be Empty");
+            return false;
+        }
+
+        else
+        {
+            regName.setError(null);
+            return true;
+
+        }
+    }
+
+    private Boolean validateEmail(){
+        String val = regMail.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.[a-z]+";
+
+        if(val.isEmpty()){
+            regMail.setError("Field Cannot be Empty");
+            return false;
+        }
+        else if(!val.matches(emailPattern)){
+            regMail.setError("Invalid Email");
+            return false;
+        }
+        else
+        {
+            regMail.setError(null);
+            return true;
+
+        }
+    }
+
+    private Boolean validatePhone(){
+        String val = regPhone.getEditText().getText().toString();
+
+        if(val.isEmpty()){
+            regPhone.setError("Field Cannot be Empty");
+            return false;
+        }
+        else
+        {
+            regPhone.setError(null);
+            return true;
+
+        }
+    }
+
+    private Boolean validatePassword(){
+        String val = regPassword.getEditText().getText().toString();
+        String passwordPattern = "^"            //Starts with String
+                +"(?=.*[0-9])"                  //at least one digit
+                + "(?=.*[a-z])(?=.*[A-Z])"      //at least one small and one capital alphabet
+                + "(?=.*[@#$%^&+=])"            // at least one special character
+                + "(?=\\S+$).{8,20}$";          // 8-20 characters long
+
+        if(val.isEmpty()){
+            regPassword.setError("Field Cannot be Empty");
+            return false;
+        }
+        else if(!val.matches(passwordPattern)){
+            regPassword.setError("Password too weak");
+            return false;
+        }
+        else
+        {
+            regPassword.setError(null);
+            return true;
+
+        }
+    }
+
+    public void registerUser(View view){
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference  = rootNode.getReference().child("Users");
+
+        if(!validateName() | !validateEmail() | !validatePhone() | !validatePassword()){
+            return;
+        }
+
+        String name = regName.getEditText().getText().toString();
+        String email = regMail.getEditText().getText().toString();
+        String phone = regPhone.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+
+        UserHelperClass helperClass = new UserHelperClass(name,email,phone,password);
+
+        reference.child(phone).setValue(helperClass);
+
     }
 }
